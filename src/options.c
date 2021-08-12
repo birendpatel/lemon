11ee30e6825ff66123d0cf76c494712c6201b7cc
Biren Patel <biren.dilip.patel@gmail.com>
2021-08-12 21:45:17 +0100

    add diagnostic flags

--- src/options.c
+++ src/options.c
@@ -8,12 +8,32 @@
 
 #include "options.h"
 
-enum options {
-	DIAGNOSTIC_PASS = 0,
-	DIAGNOSTIC_HEAP = 1,
-};
-
-//argp global parameters
-const char *argp_program_version = "1.0.0.1 alpha";
+//argp global parameters and docs
+const char *argp_program_version = "1.0.0.22 alpha";
 const char *argp_program_bug_address = "https://github.com/birendpatel/lemon";
 static const char *args_doc = "lemon [options] [filename]";
+static const char *doc = "This is the CLemon interpreter for the Lemon language."
+
+//options array indices
+enum {
+	DIAGNOSTIC_ALL,
+	DIAGNOSTIC_PASS,
+	DIAGNOSTIC_TOKENS,
+};
+
+//argp options descriptions
+static const struct argp_options options[] = {
+	[DIAGNOSTIC_ALL] = {
+		.name = "Dall",
+		.doc  = "Enable all diagnostics."
+	},
+	[DIAGNOSTIC_PASS] = {
+		.name = "Dpass",
+		.doc  = "Notify on stderr all entry and exit points for all compiler passes."
+	},
+	[DIAGNOSTIC_LEXOUT] = {
+		.name = "Dtokens",
+		.doc  = "Display tokens on stderr when lexical analysis is complete."
+	}
+}
+
