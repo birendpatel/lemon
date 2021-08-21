7d7478e554333837ee48212307e069007d5b69ee
Biren Patel <biren.dilip.patel@gmail.com>
2021-08-21 13:57:53 +0100

    add new cond var

--- src/lib/channel.h
+++ src/lib/channel.h
@@ -15,6 +15,7 @@
 
 #include <assert.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -34,14 +35,9 @@
 	#error "channel.h requires user to implement CHANNEL_EBUSY int code"
 #endif
 
-//cannot free nonempty channel without permission
-#ifndef CHANNEL_EFULL
-	#error "channel.h requires user to implement CHANNEL_EFULL int code"
-#endif
-
 //struct flags
 #define CHANNEL_OPEN	1 << 0
-#define CHANNEL_CLOSED 	1 << 1
+#define CHANNEL_CLOSED	1 << 1
 
 //typedef and forward declaration
 #define alias_channel(pfix)						       \
@@ -51,7 +47,8 @@ typdef struct pfix##_channel pfix##_channel;
 #define declare_channel(T, pfix)					       \
 struct pfix##_channel {							       \
 	pthread_mutex_t mutex;						       \
-	pthread_cond_t cond;						       \
+	pthread_cond_t cond_full;					       \
+	pthread_cont_t cond_empty;					       \
 	size_t len;							       \
 	size_t head;							       \
 	size_t tail;							       \
@@ -63,8 +60,8 @@ struct pfix##_channel {							       \
 //cls is the storage class and an optional inline directive
 #define api_channel(T, pfix, cls)					       \
 cls int pfix##_channel_init(pfix##_channel *self, const size_t n);	       \
-cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T), bool f);  \
-cls int pfix##_channel_close(pfix##_channel *self);			       \
+cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T));	       \
+cls void pfix##_channel_close(pfix##_channel *self);			       \
 cls int pfix##_channel_send(pfix##_channel *self, const T datum);	       \
 cls int pfix##_channel_recv(pfix##_channel *self, T *datum);
 
@@ -90,7 +87,8 @@ cls int pfix##_channel_init(pfix##_channel *self, const size_t n)	       \
 	self->tail = 0;							       \
 									       \
 	pthread_mutex_init(&self->mutex, NULL);				       \
-	pthread_cond_init(&self->cond, NULL);				       \
+	pthread_cond_init(&self->cond_full, NULL);			       \
+	pthread_cond_init(&self->cond_empty, NULL);			       \
 									       \
 	flags = CHANNEL_OPEN;						       \
 									       \
@@ -99,52 +97,76 @@ cls int pfix##_channel_init(pfix##_channel *self, const size_t n)	       \
 
 /*******************************************************************************
  * @def impl_channel_free
- * @brief Destroy a channel, destroy its mutex and condition variables, and
- * release any system resources. 
- * @param f Force flag; if false then channel will not be freed if there are
- * elements currently in the channel.
- * @returns If any threads are waiting ofr a broadcast, then the function fails
- * and returns CHANNEL_EBUSY. If the force flag is set and the channel is non-
- * empty, the function returns CHANNEL_EFULL. In both situations, the channel
- * will remain operable. Otherwise, CHANNEL_ESUCCESS is returns and the flag
- * CHANNEL_CLOSED will be set.
+ * @brief Destroy a channel and release system resources.
+ * @param cfree Invoked on each element in the channel unless NULL.
+ * @retruns If any threads are waiting for a signal, then CHANNEL_EBUSY is
+ * returned and the channel is not destroyed. Otherwise, CHANNEL_ESUCCESS is
+ * returned and the CHANNEL_CLOSED flag is set.
  ******************************************************************************/
 #define impl_channel_free(T, pfix, cls)					       \
-cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T), bool f)   \
-{
-	assert(self);
-
-	int err = CHANNEL_ESUCCESS;
-
-	pthread_mutex_lock(&self->mutex);
-
-	if (f && self->len > 0) {
-		err = CHANNEL_EFULL;
-		goto unlock;
-	}
-
-	err = pthread_cond_destroy(&self->cond);
-
-	if (err) {
-		err = CHANNEL_EBUSY;
-		goto unlock;
-	}
-
-	if (cfree) {
-		for (size_t i = self->head; i < self->len; i++) {
-			cfree(self->data[i]);
-		}
-	}
-
-	free(self->data);
-	memset(self, 0, sizeof(struct pfix##_channel));
-	self->flags = CHANNEL_CLOSED;
-
-unlock:
-	pthread_mutex_unlock(&self->mutex);
-	return err;
+cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T))	       \
+{									       \
+	assert(self);							       \
+									       \
+	int err = CHANNEL_ESUCCESS;					       \
+								               \
+	pthread_mutex_lock(&self->mutex);				       \
+									       \
+	err = pthread_cond_destroy(&self->cond_full);			       \
+									       \
+	if (err) {							       \
+		err = CHANNEL_EBUSY;					       \
+		goto unlock;						       \
+	}								       \
+									       \
+	err = pthread_cond_destroy(&self->cond_empty);			       \
+									       \
+	if (err) {							       \
+		err = CHANNEL_EBUSY;					       \
+		goto unlock;						       \
+	}								       \
+								               \
+	if (cfree) {							       \
+		for (size_t i = self->head; i < self->len; i++) {	       \
+			cfree(self->data[i]);				       \
+		}							       \
+	}								       \
+									       \
+	free(self->data);						       \
+	memset(self, 0, sizeof(struct pfix##_channel));			       \
+	self->flags = CHANNEL_CLOSED;				   	       \
+									       \
+unlock:									       \
+	pthread_mutex_unlock(&self->mutex);				       \
+	return err;							       \
 }
 
+/*******************************************************************************
+ * @def impl_channel_close
+ * @brief Set a flag on the channel which indicates that it will no longer
+ * receive data from producers. This does not imply that the channel is empty.
+ ******************************************************************************/
+#define impl_channel_close(T, pfix, cls)				       \
+cls void pfix##_channel_close(pfix##_channel *self) 			       \
+{									       \
+	assert(self);							       \
+									       \
+	pthread_mutex_lock(&self->mutex);				       \
+									       \
+	self->flags = CHANNEL_CLOSED;					       \
+									       \
+	pthread_mutex_unlock(&self->mutex);				       \
+}
+
+/*******************************************************************************
+ * @def impl_channel_send
+ * @brief Place an item at the back of the channel queue.
+ ******************************************************************************/
+#define impl_channel_send(T, pfix, cls)					       \
+cls int pfix##_channel_send(pfix##_channel *self, const T datum) 	       \
+{
+	...
+}
 
 /*******************************************************************************
  * @def make_channel
