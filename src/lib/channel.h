78b31b8567a5f7489f38f228195ea20922f55ee6
Biren Patel <biren.dilip.patel@gmail.com>
2021-08-21 17:03:40 +0100

    implement send function

--- src/lib/channel.h
+++ src/lib/channel.h
@@ -1,6 +1,7 @@
 /**
  * @file channel.h
  * @author Copyright (C) 2021 Biren Patel. GNU General Public License v3.0.
+ *
  * @brief This data structure is a simple multi-producer multi-consumer FIFO
  * thread-safe queue implemented via posix threads. The queue is blocking and
  * its buffer size is fixed to enable the producers to place back-pressure
@@ -30,9 +31,14 @@
 	#error "channel.h requires user to implement CHANNEL_ENOMEM int code"
 #endif
 
-//cannot free channel, threads are waiting
+//one or more threads are waiting
 #ifndef CHANNEL_EBUSY
 	#error "channel.h requires user to implement CHANNEL_EBUSY int code"
+#endif
+
+//attempted to send on a closed channel or recv on a closed empty channel
+#ifndef CHANNEL_ECLOSED
+	#error "channel.h requires user to implement CHANNEL_ECLOSED int code"
 #endif
 
 //struct flags
@@ -50,6 +56,7 @@ struct pfix##_channel {							       \
 	pthread_cond_t cond_full;					       \
 	pthread_cont_t cond_empty;					       \
 	size_t len;							       \
+	size_t cap;
 	size_t head;							       \
 	size_t tail;							       \
 	T *data;							       \
@@ -67,7 +74,7 @@ cls int pfix##_channel_recv(pfix##_channel *self, T *datum);
 
 /*******************************************************************************
  * @def impl_channel_init
- * @brief Initialize a channel with a fixed length n > 0.
+ * @brief Initialize a channel with a fixed capacity n > 0.
  * @details This function must be invoked before any other channel functions.
  ******************************************************************************/
 #define impl_channel_init(T, pfix, cls)					       \
@@ -82,7 +89,8 @@ cls int pfix##_channel_init(pfix##_channel *self, const size_t n)	       \
 		return CHANNEL_ENOMEM;					       \
 	}								       \
 									       \
-	self->len = n;							       \
+	self->cap = n;							       \
+	self->len = 0;							       \
 	self->head = 0;							       \
 	self->tail = 0;							       \
 									       \
@@ -109,7 +117,7 @@ cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T))	       \
 	assert(self);							       \
 									       \
 	int err = CHANNEL_ESUCCESS;					       \
-								               \
+									       \
 	pthread_mutex_lock(&self->mutex);				       \
 									       \
 	err = pthread_cond_destroy(&self->cond_full);			       \
@@ -125,7 +133,7 @@ cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T))	       \
 		err = CHANNEL_EBUSY;					       \
 		goto unlock;						       \
 	}								       \
-								               \
+									       \
 	if (cfree) {							       \
 		for (size_t i = self->head; i < self->len; i++) {	       \
 			cfree(self->data[i]);				       \
@@ -134,7 +142,7 @@ cls int pfix##_channel_free(pfix##_channel *self, void (*cfree) (T))	       \
 									       \
 	free(self->data);						       \
 	memset(self, 0, sizeof(struct pfix##_channel));			       \
-	self->flags = CHANNEL_CLOSED;				   	       \
+	self->flags = CHANNEL_CLOSED;					       \
 									       \
 unlock:									       \
 	pthread_mutex_unlock(&self->mutex);				       \
@@ -147,7 +155,7 @@ unlock:									       \
  * receive data from producers. This does not imply that the channel is empty.
  ******************************************************************************/
 #define impl_channel_close(T, pfix, cls)				       \
-cls void pfix##_channel_close(pfix##_channel *self) 			       \
+cls void pfix##_channel_close(pfix##_channel *self)			       \
 {									       \
 	assert(self);							       \
 									       \
@@ -160,12 +168,38 @@ cls void pfix##_channel_close(pfix##_channel *self) 			       \
 
 /*******************************************************************************
  * @def impl_channel_send
- * @brief Place an item at the back of the channel queue.
+ * @brief Place an item at the back of the channel.
+ * @details Thread will suspend without timeout if the channel is full.
  ******************************************************************************/
 #define impl_channel_send(T, pfix, cls)					       \
-cls int pfix##_channel_send(pfix##_channel *self, const T datum) 	       \
+cls int pfix##_channel_send(pfix##_channel *self, const T datum)	       \
 {
-	...
+	assert(self);
+
+	int err = CHANNEL_ESUCCESS;
+
+	pthread_mutex_lock(&self->mutex);
+
+	if (self->flags & CHANNEL_CLOSED) {
+		err = CHANNEL_ECLOSED;
+		goto unlock;
+	}
+
+	while (self->len == self->cap) {
+		pthread_cond_wait(&self->cond_full, &self->mutex);
+	}
+
+	self->data[self->tail] = datum;
+	self->tail = (self->tail + 1) % self->cap;
+	self->len++;
+
+	if (self->len == 1) {
+		pthread_cond_signal(&self->cond_empty);
+	}
+
+unlock:
+	pthread_mutex_unlock(&self->mutex);
+	return err;
 }
 
 /*******************************************************************************
