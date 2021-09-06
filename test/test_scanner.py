d91bb1508c378044cea52a61cec61a5ef775a4a0
Biren Patel <biren.dilip.patel@gmail.com>
2021-09-06 19:37:48 +0100

    begin scan tests

--- test/test_scanner.py
+++ test/test_scanner.py
@@ -1,5 +1,7 @@
 # Copyright (C) 2021 Biren Patel. GNU General Public License v3.0.
-# Tokenization tests
+# These tokenization tests capture the token output from stderr when Lemon
+# is executed with the --Dtokens flag. The token type is extracted from
+# the captured output.
 
 from subprocess import run
 
