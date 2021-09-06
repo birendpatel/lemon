d5d84f4c649405dbe124cf29c22f230648fd77c8
Biren Patel <biren.dilip.patel@gmail.com>
2021-09-07 00:32:52 +0100

    scanner unit test done

--- test/test_scanner.py
+++ test/test_scanner.py
@@ -8,39 +8,130 @@ import unittest
 from re import search
 from subprocess import run
 
-# returns a list of types, in the order that they occur, from the input
-def isolate(text: str):
-    lines = text.split('\n')
-
-    tokens = [i for i in lines if i.find("TOKEN") != -1 ]
-
-    types = []
-
-    # fetch the region of text in the token which is between the two colons
-    # and then strip those colons along with any inner whitespace
-    for i in tokens:
-        match = search(r":[^:]+:", i)
-
-        if match:
-            types.append(match.group()[1:-1].strip())
-
-    return types
-
 class TestTokenizer(unittest.TestCase):
     def test_all_tokenization_cases_from_file_input(self):
         #arrange
+        self.maxDiff = None
+
         cmd = "../debug/lemon --Dtokens ./test_scanner.lem"
+
+        expected = [
+            "IDENTIFIER",
+            "IDENTIFIER",
+            "IDENTIFIER",
+            "IDENTIFIER",
+            "SEMICOLON",
+            "INT LITERAL",
+            "INT LITERAL",
+            "INT LITERAL",
+            "INT LITERAL",
+            "SEMICOLON",
+            "FLOAT LITERAL",
+            "FLOAT LITERAL",
+            "FLOAT LITERAL",
+            "FLOAT LITERAL",
+            "FLOAT LITERAL",
+            "FLOAT LITERAL",
+            "SEMICOLON",
+            "STRING LITERAL",
+            "STRING LITERAL",
+            "STRING LITERAL",
+            "SEMICOLON",
+            "COLON",
+            "LEFT BRACKET",
+            "RIGHT BRACKET",
+            "LEFT PARENTHESIS",
+            "RIGHT PARENTHESIS",
+            "LEFT BRACE",
+            "RIGHT BRACE",
+            "DOT",
+            "TILDE",
+            "COMMA",
+            "SEMICOLON",
+            "EQUAL",
+            "EQUAL EQUAL",
+            "NOT EQUAL",
+            "NOT",
+            "AND",
+            "OR",
+            "BITWISE NOT",
+            "AMPERSAND",
+            "BITWISE OR",
+            "BITWISE XOR",
+            "LEFT SHIFT",
+            "RIGHT SHIFT",
+            "GREATER THAN",
+            "GREATER OR EQUAL",
+            "LESS THAN",
+            "LESS OR EQUAL",
+            "ADD",
+            "MINUS",
+            "STAR",
+            "DIVISION",
+            "MODULO",
+            "FOR LOOP",
+            "WHILE LOOP",
+            "BREAK",
+            "CONTINUE",
+            "IF BRANCH",
+            "ELSE BRANCH",
+            "SWITCH",
+            "CASE",
+            "FALLTHROUGH",
+            "GOTO",
+            "LET",
+            "MUT",
+            "NULL",
+            "BOOL TRUE",
+            "BOOL FALSE",
+            "STRUCT",
+            "FUNC",
+            "PRIVATE",
+            "PUBLIC",
+            "RETURN",
+            "SELF",
+            "VOID",
+            "EQUAL EQUAL", "EQUAL",
+            "NOT EQUAL", "EQUAL",
+            "NOT EQUAL", "EQUAL EQUAL", "EQUAL",
+            "AND", "AMPERSAND",
+            "OR", "BITWISE OR",
+            "LEFT SHIFT", "LESS THAN",
+            "RIGHT SHIFT", "GREATER THAN",
+            "LEFT SHIFT", "LESS OR EQUAL",
+            "RIGHT SHIFT", "GREATER OR EQUAL",
+            "GREATER OR EQUAL", "EQUAL EQUAL",
+            "GREATER OR EQUAL", "EQUAL EQUAL", "EQUAL",
+            "LESS OR EQUAL", "EQUAL EQUAL",
+            "LESS OR EQUAL", "EQUAL EQUAL", "EQUAL"
+        ]
 
         #act
         proc_output = run(cmd, check=True, shell=True, capture_output=True)
         text = proc_output.stderr.decode()
-
-        types = isolate(text)
-
-        print(types)
+        types = self.isolate(text)
 
         #assert
-        self.assertTrue(1 == 1)
+        self.assertListEqual(types, expected)
+    
+    def isolate(self, text: str):
+        """return a list of token types in order of appearance"""
+        lines = text.split('\n')
+
+        tokens = [i for i in lines if i.find("TOKEN") != -1 ]
+
+        types = []
+
+        # fetch the region of text in the token which is between the two colons
+        # and then strip those colons along with any inner whitespace
+        for i in tokens:
+            match = search(r":[^:]+:", i)
+
+            if match:
+                types.append(match.group()[1:-1].strip())
+
+        return types
+
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
