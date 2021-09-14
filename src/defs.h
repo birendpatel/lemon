8ef5261eddff96a2dd646cf0bff91565ab9de6e3
Biren Patel <biren.dilip.patel@gmail.com>
2021-09-14 23:51:18 +0100

    added kmalloc def

--- src/defs.h
+++ src/defs.h
@@ -14,3 +14,25 @@
 
 //wrapper for gcc fallthrough extension
 #define fallthrough __attribute__((fallthrough))
+
+/*******************************************************************************
+ * @def kmalloc
+ *
+ * @details For the Lemon compiler, there is no situation where the program
+ * will enter a recovery path and successfully sidestep a malloc failure. If
+ * we were to check the malloc code, in all cases it would get passed up the 
+ * call stack to main where a fatal buffer flush in xerror would be triggered.
+ *
+ * In many areas of the compiler (i.e., the parser), malloc is used so often
+ * that checking the return value absolutely destroys code readability. It
+ * offers very, very little benefit in return.
+ *
+ * I learned the following trick from Jens Gustedt's blog. If malloc fails,
+ * memset attempts to write a 0 to the first byte at NULL. It fails fast and
+ * it fails early. Since the return value of malloc is not known at compile
+ * time, it also avoids triggering a -Wnonull warning from the GCC compiler.
+ *
+ * Also, note that this header doesn't automatically include the strings.h and
+ * stdlib.h headers.
+ ******************************************************************************/
+#define kmalloc(target, bytes) memset((target = malloc(bytes)), 0, 1)
