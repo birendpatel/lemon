d268c9fa89599f2195ecb35a7428e3497b1334ae
Biren Patel <biren.dilip.patel@gmail.com>
2021-10-27 15:40:22 +0100

    remove repl, add DAG notes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,21 @@
 // Copyright (C) 2021 Biren Patel. GNU General Public License v3.0.
 //
-// main.c is responsible for orchestrating the major compiler phases: parsing,
-// static analysis, optimisation, and code generation. The file handles all
-// initialisation and cleanup required before, after, and between each phase.
+// main.c compiles lemon source code into bytecode and executes it on a virtual
+// machine.
+//
+// The program begins by parsing the input file into an abstract syntax tree.
+// A directed dependency graph is then created recursively depth-first by
+// analysing the import statements. Graph nodes represent the imported files
+// and directed edges represent dependencies.
+//
+// Once the graph is finalized, the program verifies that it is acyclic. Then,
+// a symbol table is created for each graph node in topological order. Semantic
+// analysis is carried on each node independently.
+//
+// Finally, the optimisation and bytecode generation phases are performed and
+// the output program is sent to the virtual machine for execution. The virtual
+// machine controls all runtime aspects including garbage collection, overflow
+// detection, and debugging.
 
 #include <assert.h>
 #include <errno.h>
@@ -23,12 +36,8 @@
 #endif
 
 void ShowHeader(void);
-uint64_t Xorshift64(const uint64_t);
-cstring *GenerateUniqueAlias(void);
-xerror ExecuteFromRepl(void);
-cstring *ReadStandardInput(void);
-xerror ExecuteShell(const cstring *);
 void FileCleanup(FILE **);
+xerror LoadFile(const cstring *);
 xerror ExecuteFromFiles(const int, char **); 
 cstring *cStringFromFile(FILE *, xerror *);
 xerror GetFilesize(FILE *, size_t *);
@@ -41,13 +50,22 @@ int main(int argc, char **argv)
 {
 	OptionsParse(&argc, &argv);
 
-	xerror err = XEUNDEFINED;
+	const cstring *input_file = argv[0];
 
-	if (argv[argc]) {
-		err = ExecuteFromFiles(argc, argv);
-	} else {
-		err = ExecuteFromRepl();
+	if (!input_file) {
+		const cstring *msg = "no input files; nothing to compile";
+		XerrorUser(0, msg);
+		return EXIT_FAILURE;
 	}
+
+	const cstring *sentinel = argv[1];
+
+	if (sentinel != NULL) {
+		const cstring *msg = "all input files except %s were ignored";
+		XerrorUser(0, msg, input_file);
+	}
+
+	xerror err = LoadFile(input_file);
 
 	if (err) {
 		xerror_fatal("%s", XerrorDescription(err));
@@ -58,147 +76,6 @@ int main(int argc, char **argv)
 	return EXIT_SUCCESS;
 }
 
-//------------------------------------------------------------------------------
-//repl management
-
-xerror ExecuteFromRepl(void)
-{
-	ShowHeader();
-
-	xerror err = XEUNDEFINED;
-	const xerror fatal_errors = ~(XESHELL | XEPARSE);
-
-	RAII(cStringFree) cstring *alias = GenerateUniqueAlias();
-	RAII(cStringFree) cstring *input = ReadStandardInput();
-
-	while (input) {
-		if (input[0] == '$') {
-			err = ExecuteShell(input);
-		} else {
-			err = Compile(input, alias);
-		}
-
-		if (err & fatal_errors) {
-			xerror_issue("cannot execute via REPL");
-			return err;
-		}
-
-		input = ReadStandardInput();
-	}
-
-	return XESUCCESS;
-}
-
-//signal interrupt returns NULL, else returns a dynamically allocated cstring
-cstring *ReadStandardInput(void)
-{
-	vstring buffer = vStringInit(KiB(1));
-
-	int prev = 0;
-	int curr = 0;
-
-	printf(">>> ");
-
-	while (true) {
-		fflush(stdout);
-
-		curr = getchar(); 
-
-		switch(curr) {
-		case EOF:
-			printf("\n");
-			return NULL;
-
-		case '\n':
-			if (prev == '\n') {
-				vStringTrim(&buffer, '\n');
-				return cStringFromvString(&buffer);
-			} 
-			
-			printf("... ");
-			fallthrough;
-
-		default:
-			vStringAppend(&buffer, (char) curr);
-			prev = curr;
-			break;
-		}
-	}
-}
-
-void ShowHeader(void)
-{
-	static const cstring *msg =
-		"Lemon (%s) (Compiled %s %s)\n"
-		"Copyright (C) 2021 Biren Patel.\n"
-		"GNU General Public License v3.0.\n\n"
-		"- Double tap 'return' to execute source code.\n"
-		"- Prefix input with '$' to execute a shell command.\n"
-		"- Exit with Ctrl-C.\n\n";
-
-	fprintf(stdout, msg, LEMON_VERSION, __DATE__, __TIME__);
-}
-
-//Marsaglia, G. (2003). Xorshift RNGs. Journal of Statistical Software, 8(14),
-//1–6. https://doi.org/10.18637/jss.v008.i14
-uint64_t Xorshift64(const uint64_t curr)
-{
-	uint64_t next = curr;
-
-	next = next  << 13;
-	next = next  >> 7;
-	next = next  << 17;
-
-	return next;
-}
-
-//returns a dynamically allocated cstring
-cstring *GenerateUniqueAlias(void)
-{
-	enum {
-		max_digits = 64,
-		seed = 0xCAFEBEEF
-	};
-
-	static cstring template[max_digits] = "";
-	
-	static uint64_t random_number = seed;
-
-	random_number = Xorshift64(random_number);
-
-	int total = sprintf(template, "%" PRIu64, random_number);
-
-	template[total] = '\0';
-
-	return cStringDuplicate(template);
-}
-
-
-xerror ExecuteShell(const cstring *cstr)
-{
-	assert(cstr);
-	assert(cstr[0] == '$');
-
-	const cstring *cmd = NULL;
-	const cstring *sanitized_input = cstr + 1;
-
-	if (*sanitized_input) {
-		cmd = sanitized_input;
-	}
-
-	int err = system(cmd);
-
-	if (err == -1) {
-		perror(NULL);
-		return XESHELL;
-	}
-
-	return XESUCCESS;
-}
-
-//------------------------------------------------------------------------------
-//file management
-
 void FileCleanup(FILE **handle)
 {
 	if (*handle) {
@@ -206,35 +83,29 @@ void FileCleanup(FILE **handle)
 	}
 }
 
-xerror ExecuteFromFiles(const int argc, char **argv)
+xerror LoadFile(const cstring *fname)
 {
-	assert(argc);
-	assert(argv);
+	xerror err = XEUNDEFINED;
 
-	for (int i = 0; i < argc; i++) {
-		xerror err = XEUNDEFINED;
-		const cstring *fname = argv[i];
+	RAII(FileCleanup) FILE *fp = fopen(fname, "r");
 
-		RAII(FileCleanup) FILE *fhandle = fopen(fname, "r");
+	if (!fp) {
+		xerror_issue("%s: %s", fname, strerror(errno));
+		return XEFILE;
+	}
 
-		if (!fhandle) {
-			xerror_issue("%s: %s", fname, strerror(errno));
-			return XEFILE;
-		}
-	
-		RAII(cStringFree) cstring *src = cStringFromFile(fhandle, &err);
+	RAII(cStringFree) cstring *src = cStringFromFile(fp, &err);
 
-		if (err) {
-			xerror_issue("%s: cannot copy file to memory", fname);
-			return XEFILE;
-		}
+	if (err) {
+		xerror_issue("%s: cannot copy file to memory", fname);
+		return XEFILE;
+	}
 
-		err = Compile(src, fname);
+	err = Compile(src, fname);
 
-		if (err) {
-			xerror_issue("%s: cannot compile from file", fname);
-			return XEFILE;
-		}
+	if (err) {
+		xerror_issue("%s: cannot compile from file", fname);
+		return XEFILE;
 	}
 
 	return XESUCCESS;
@@ -322,22 +193,6 @@ xerror Compile(const cstring *src, const cstring *alias)
 	if (!ast) {
 		xerror_issue("cannot create abstract syntax tree");
 		return XEPARSE;
-	}
-
-	if (trace) {
-		fprintf(stderr, "compiler pass: symbol table");
-	}
-
-	if (trace) {
-		fprintf(stderr, "compiler pass: semantic analysis");
-	}
-
-	if (trace) {
-		fprintf(stderr, "compiler pass: optimisation");
-	}
-
-	if (trace) {
-		fprintf(stderr, "compiler pass: code generation");
 	}
 
 	return XESUCCESS;
