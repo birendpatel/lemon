fec03011fa261efd2893d95e01a9e8dec07ef53a
Biren Patel <biren.dilip.patel@gmail.com>
2021-11-13 12:23:56 +0000

    add tokens to scanner API

--- src/scanner.h
+++ src/scanner.h
@@ -4,10 +4,105 @@
 
 #pragma once
 
-#include "token.h"
 #include "xerror.h"
 #include "channel.h"
 #include "str.h"
+
+typedef enum token_type {
+	//markers
+	_INVALID = 0,
+	_EOF,
+	_IDENTIFIER,
+
+	//atomic literals
+	_LITERALINT,
+	_LITERALFLOAT,
+	_LITERALSTR,
+
+	//punctuation
+	_SEMICOLON,	// ;
+	_LEFTBRACKET,	// [
+	_RIGHTBRACKET,	// ]
+	_LEFTPAREN,	// (
+	_RIGHTPAREN,	// )
+	_LEFTBRACE,	// {
+	_RIGHTBRACE,	// }
+	_DOT,		// .
+	_TILDE,		// ~
+	_COMMA,		// ,
+	_COLON,		// :
+
+	//operators
+	_EQUAL,		// =
+	_EQUALEQUAL,	// ==
+	_NOTEQUAL,	// !=
+	_NOT,		// !
+	_AND,		// &&
+	_OR,		// ||
+	_BITNOT,	// '
+	_AMPERSAND,	// &
+	_BITOR,		// |
+	_BITXOR,	// ^
+	_LSHIFT,	// <<
+	_RSHIFT,	// >>
+	_GREATER,	// >
+	_GEQ,		// >=
+	_LESS,		// <
+	_LEQ,		// <=
+	_ADD,		// +
+	_MINUS,		// -
+	_STAR,		// *
+	_DIV,		// /
+	_MOD,		// %
+
+	//sequence point keywords; file level
+	_STRUCT,
+	_FUNC,
+	_METHOD,
+	_LET,
+
+	//sequence point keywords; block level
+	_RETURN,
+	_BREAK,
+	_CONTINUE,
+	_GOTO,
+	_FOR,
+	_WHILE,
+	_IF,
+	_SWITCH,
+	
+	//regular keywords
+	_ELSE,
+	_CASE,
+	_DEFAULT,
+	_FALLTHROUGH,
+	_LABEL,
+	_MUT,
+	_NULL,
+	_TRUE,
+	_FALSE,
+	_PUB,
+	_SELF,
+	_VOID,
+	_IMPORT,
+
+	//total
+	_TOKEN_TYPE_COUNT
+} token_type;
+
+typedef struct token_flags {
+	unsigned int valid : 1;
+	unsigned int bad_string : 1; //valid when type == _LITERALSTR
+} token_flags;
+
+typedef struct token {
+	cstring *lexeme; //either dynamically allocated or NULL
+	token_type type;
+	size_t line; //starts at 1
+	token_flags flags;
+} token;
+
+#define INVALID_TOKEN (token) {NULL, _INVALID, 0, {0, 0}}
 
 //------------------------------------------------------------------------------
 //Tokens are sent on the channel in the order that they are found. On completion
