0234fac8b536945e72e11238c806a958e0204145
Biren Patel <biren.dilip.patel@gmail.com>
2021-11-25 15:11:23 +0000

    refactor arena API

--- src/utils/arena.c
+++ src/utils/arena.c
@@ -1,8 +1,7 @@
 // Copyright (C) 2021 Biren Patel. GNU General Public License v3.0.
 
 #include <assert.h>
-#include <stdbool.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "arena.h"
@@ -17,15 +16,33 @@
 typedef struct arena arena;
 typedef struct header header;
 
-static header *GetHeader(void *);
-static size_t Align(const size_t);
+//configurable to any power of two
+#define ALIGNMENT ((size_t) 0x10)
+
+static size_t Align(const size_t bytes)
+{
+	assert(ALIGNMENT != 0 && "degenerate alignment");
+	assert(SIZE_MAX % ALIGNMENT == 0 && "potential overflow");
+	assert(!(ALIGNMENT & (ALIGNMENT - 1)) && "not a power of 2");
+
+	const size_t mask = ALIGNMENT - 1;
+	const size_t offset = (ALIGNMENT- (bytes & mask)) & mask;
+
+	const size_t adjustment = bytes + offset;
+
+	if (bytes != adjustment) {
+		ArenaTrace("aligned %zu to %zu", bytes, adjustment);
+	}
+
+	return adjustment;
+}
 
 //------------------------------------------------------------------------------
-//an arena is just a contiguous block of stdlib malloced memory with an sbrk()
-//style increment that progressively bumps the top pointer. Once the top pointer
-//is at (char*) arena.top + capacity, the arena is out of memory. An arena is 
-//created for each thread in its TLS; this mitigates thread contention that 
-//would occur if we instead used a single shared arena with mutex locks.
+//an arena is a contiguous block of calloced memory with an sbrk() style pointer
+//that progressively bumps in multiples of 16. If the top pointer reaches the
+//location at (char*) arena.top + capacity, then the arena is out of memory. The
+//GCC storage class __thread (_Thread_local in C11) substitutes in for a more
+//cumbersome pthread_key_t implementation.
 
 struct arena {
 	void *top;
@@ -40,7 +57,7 @@ static __thread arena arena_tls =  {
 };
 
 //------------------------------------------------------------------------------
-//a header hides just in front of each memory region returned to the user and is
+//a header hides just in front of each memory region returned to the user; it is
 //the primary mechanism that enables block reallocation.
 
 struct header {
@@ -52,83 +69,70 @@ static header *GetHeader(void *user_region)
 	assert(user_region);
 
 	const size_t offset = sizeof(header);
+
 	return (header *) ((char *) user_region - offset);
 }
 
 //------------------------------------------------------------------------------
-//note; since arena size can be modified via compiler arguments, the bounds
-//check must not be asserted since it will reduce to a no-op in release mode.
 
-void ArenaInit(size_t bytes)
+bool ArenaInit(size_t bytes)
 {
-	if (!bytes) {
-		xerror_fatal("requested empty arena");
-		abort();
-	}
+	ArenaTrace("request for arena with %zu bytes", bytes);
 
-	arena_tls.top = calloc(bytes, 1);
-
-	if (!arena_tls.top) {
-		xerror_fatal("cannot allocate memory for new arena");
-		abort();
-	}
+	bytes = Align(bytes);
 
 	arena_tls.capacity = bytes;
 	arena_tls.remaining = bytes;
+	arena_tls.top = calloc(bytes, 1);
 
-	__attribute__((unused))
-	const cstring *msg = "initialised arena at %p with %zu bytes";
+	if (!arena_tls.top) {
+		xerror_fatal("stdlib calloc; out of memory");
+		return false;
+	}
 
-	ArenaTrace(msg, arena_tls.top, bytes);
+	ArenaTrace("initialised arena at %p", arena_tls.top);
+	
+	return true;
 }
 
 void ArenaFree(void)
 {
-	assert(arena_tls.top);
+	if (!arena_tls.top) {
+		xerror_issue("thread local arena not initialised");
+		return;
+	}
 
 	const size_t offset = arena_tls.capacity - arena_tls.remaining;
 	void *start = (char *) arena_tls.top - offset;
-
-	assert(start);
 	free(start);
 
-	__attribute__((unused))
-	const double used = 
-		(double) arena_tls.remaining / (double) arena_tls.capacity;
-	
-	ArenaTrace("allocation at %p released (%g%%)", start, used);
-}
+	__attribute__((unused)) double total = (double) arena_tls.capacity;
+	__attribute__((unused)) double used = (double) arena_tls.remaining;
+	__attribute__((unused)) double percent = used / total;
 
-//------------------------------------------------------------------------------
-
-//returns input rounded up to nearest multiple of 16
-static size_t Align(const size_t bytes)
-{
-	const size_t alignment = 0x10;
-	const size_t mask = alignment - 1;
-	const size_t newsize = bytes + ((alignment - (bytes & mask)) & mask);
-
-	assert(newsize >= bytes && "alignment overflow");
-
-	return newsize;
+	ArenaTrace("allocation at %p released (%g%%)", start, percent);
 }
 
 void *ArenaAllocate(size_t bytes)
 {
-	assert(arena_tls.top);
-	assert(bytes);
+	if (!arena_tls.top) {
+		xerror_fatal("thread local arena not initialised");
+		return NULL;
+	}
 
-	ArenaTrace("request for %zu bytes", bytes);
+	ArenaTrace("request for new block with %zu bytes", bytes);
 
 	const size_t user_bytes = Align(bytes);
 	const size_t total_bytes = sizeof(header) + user_bytes;
-	assert(total_bytes > user_bytes && "region + header causes overflow");
 
-	ArenaTrace("changed to %zu bytes (total %zu)", user_bytes, total_bytes);
+	if (total_bytes < user_bytes) {
+		xerror_fatal("region + header causes overflow");
+		return NULL;
+	}
 
 	if (total_bytes > arena_tls.remaining) {
-		xerror_fatal("arena is out of memory");
-		abort();
+		xerror_fatal("arena; out of memory");
+		return NULL;
 	}
 
 	header *metadata = arena_tls.top;
@@ -137,33 +141,45 @@ void *ArenaAllocate(size_t bytes)
 	arena_tls.top = (void *) ((char *) arena_tls.top + total_bytes);
 	arena_tls.remaining -= total_bytes;
 
-	ArenaTrace("done; %zu bytes remain", arena_tls.remaining);
+	ArenaTrace("request fulfilled; block header at %p", (void *) metadata);
+	ArenaTrace("arena; %zu bytes remain", arena_tls.remaining);
 
 	void *user_region = metadata + 1;
 	return user_region;
 }
 
-void *ArenaReallocate(void *ptr, size_t bytes)
+void *ArenaReallocate(void *old, size_t bytes)
 {
-	assert(arena_tls.top);
-	assert(ptr);
-	assert(bytes);
+	if (!arena_tls.top) {
+		xerror_fatal("thread local arena not initialised");
+		return NULL;
+	}
+
+	if (!old) {
+		xerror_fatal("request to reallocate NULL block");
+		return NULL;
+	}
 
 	void *new = NULL;
-	header *metadata = GetHeader(ptr);
+	header *metadata = GetHeader(old);
 
-	ArenaTrace("reallocation of %zu bytes requested", bytes);
+	ArenaTrace("request; realloc %p to %zu bytes", (void *) metadata, bytes);
 
 	if (metadata->bytes >= bytes) {
-		ArenaTrace("no realloc; block has %zu bytes", metadata->bytes);
-		return ptr;
+		ArenaTrace("denied; block has %zu bytes", metadata->bytes);
+		return old;
 	}
 
 	new = ArenaAllocate(bytes);
 
-	memcpy(new, ptr, metadata->bytes);
+	if (!new) {
+		xerror_fatal("new block request failed");
+		return NULL;
+	}
 
-	ArenaTrace("reallocated from %p to %p", ptr, new);
+	memcpy(new, old, metadata->bytes);
+
+	ArenaTrace("data copied from user block at %p to %p", old, new);
 
 	return new;
 }
--- src/utils/arena.h
+++ src/utils/arena.h
@@ -1,32 +1,56 @@
 // Copyright (C) 2021 Biren Patel. GNU General Public License v3.0.
 //
-// Thread-safe arena allocator with 16-byte alignment. Like compilers such as
-// DMD and 8cc, C Lemon's allocator does not provide a mechanism to free the
-// allocated blocks. This means that memory does not need to be managed, and so
-// application code can be written as if a garbage collector is present. This
-// also eliminates use-after-free and double-free bugs.
+// Thread-safe arena memory allocator. Each arena is created in thread local
+// storage to eliminate contention. Like the DMD and 8cc compilers, this
+// "set it and forget it" allocation policy results in simpler application
+// code, a faster compiler, and fewer use-after-free & double-free bugs.
 
 #pragma once
 
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 #define KiB(kibibytes) ((size_t) (1024 * kibibytes))
 #define MiB(mebibytes) ((size_t) (1048576 * mebibytes))
 #define GiB(gibibytes) ((size_t) (1073741824 * gibibytes))
 
-//initialize the arena with maximum capacity == bytes; will abort() if the 
-//internal stdlib allocation call fails or bytes == 0.
-void ArenaInit(size_t bytes);
+//initialise a thread-local arena with a minimum fixed capacity of 'bytes';
+//returns false on failure
+bool ArenaInit(size_t bytes);
 
-//The call will abort if bytes == 0, if any internal adjustments to the input
-//request cause an overflow, or if the request is more than the remaining free 
-//bytes. Otherwise returns a zeroed region of memory.
+//returns an aligned and zeroed memory block; returns NULL on failure
 void *ArenaAllocate(size_t bytes);
 
-//The call will abort if bytes == 0 or if the request is more than the remaining
-//free bytes. The call is equivalent to ArenaAllocate() if ptr == NULL. Returns 
-//a zeroed region of memory which might be identical to the input region.
+//reallocate the data at 'ptr' to an aligned block of memory; if the new size
+//is larger than the old size, then the additional memory is zeroed; returns
+//NULL on failure
 void *ArenaReallocate(void *ptr, size_t bytes);
 
-//releases system resources acquired by ArenaInit
+//releases system resources acquired by ArenaInit; returns false if arena was
+//not initialised prior to this call
 void ArenaFree(void);
+
+__attribute__((always_inline))
+static inline void *allocate(size_t bytes)
+{
+	void *ptr = ArenaAllocate(bytes);
+
+	if (!ptr) {
+		abort();
+	}
+
+	return ptr;
+}
+
+__attribute__((always_inline))
+static inline void *reallocate(void *ptr, size_t bytes)
+{
+	void *new = ArenaReallocate(ptr, bytes);
+
+	if (!new) {
+		abort();
+	}
+
+	return new;
+}
